4e5f56ba68d8fc84810ba897ce527b41ad77abfd
Moksh <Your email Address>
2024-04-15 14:07:05 +0530

    hi

--- data_analysis_project/data_analysis_project/settings.py
+++ data_analysis_project/data_analysis_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2f@w8f1#4(y3$tpa&$-s9wdw01kg5!+cwg#=bbr6h^ripbx-sk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -117,6 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
